ec936024ca22332606f2bc8e0452e0dbb7f2f7fc
Goodnewsss455 <goodnewsss001@accountant.com>
2025-11-10 15:51:50 +0600

    Create jsdelivr-header.js

---
+++ cdn.jsdelivr.net/sm/jsdelivr-header.js
@@ -0,0 +1,6 @@
+/**
+ * Minified by jsDelivr using Terser v5.37.0.
+ * Original file: /npm/vue@3.2.31/dist/vue.global.js
+ *
+ * Do NOT use SRI with dynamically generated files! More information: https://www.jsdelivr.com/using-sri-with-dynamic-files
+ */
